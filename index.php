71303ec26963af4a4db82d01b0dbec951f1440d9
Simon <Simon@Simon-PC>
2015-09-09 14:59:00 +0200

    added newpong.js

--- index.php
+++ index.php
@@ -12,66 +12,21 @@ and open the template in the editor.
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script src="/js/scripts.js" type="text/javascript"></script>
-        <script type="text/javascript" src="js/Chart.min.js"></script>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <!-- Optional theme -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
         
         <!-- custom css -->
-        <link rel="stylesheet" href="css/styles.css">
+        
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         <!-- custom js -->
-        <script src="js/pong.js"></script>
+        <script src="js/newpong.js"></script>
     </head>
     <body>
 
-        <div class="container-fluid">
-            <nav class="navbar navbar-default" role="navigation">
-   <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-         <span class="sr-only">Toggle navigation</span>
-         <span class="icon-bar"></span>
-         <span class="icon-bar"></span>
-         <span class="icon-bar"></span>
-      </button>
-   </div>
-   <div class="collapse navbar-collapse  navbar-ex1-collapse">
-      <ul class="nav navbar-nav">
-         <li class="text-center"><a href="index.php">Pong</a></li>
-         <li><a class="text-center" href="about.php">About</a></li>   
-      </ul>
-  </div>
-            </nav>
-            <!-- This can't possibly be how you are meant to design with columns in bootstrap ? ?? !! ?? -->
-            <div class="row">
-                <div class="col-sm-4">
-                    
-                </div>
-                <div class="col-sm-4">
-                    <p>Pong</p>
-                </div>
-                <div class="col-sm-4">
-                   
-                </div>
-                
-            </div>   
-            <div class="row">
-                <div class="col-sm-4">
-                   
-                </div>
-                <div class="col-sm-4">
-                    <!-- Draw the pong game in this canvas -->
-                    <canvas id="canvas" width="450" height="450"></canvas>
-                </div>
-                <div class="col-sm-4">
-                
-                </div>
-                
-            </div>
-        </div>
+        
 
     </body>
 </html>
---
+++ js/newpong.js
@@ -0,0 +1,83 @@
+var WIDTH = 700, HEIGHT = 600, pi = Math.PI, UpArrow = 38, DownArrow = 40, canvas, ctx, keystate;
+var player = new Paddle(100, 100, false);
+var ai = new Paddle(0, WIDTH - 40, true);
+
+
+function init() {
+
+}
+function main() {
+    // create, initiate and append game canvas
+    canvas = document.createElement("canvas");
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+    ctx = canvas.getContext("2d");
+    document.body.appendChild(canvas);
+    keystate = {};
+    // keep track of keyboard presses
+    document.addEventListener("keydown", function (evt) {
+        keystate[evt.keyCode] = true;
+    });
+    document.addEventListener("keyup", function (evt) {
+        delete keystate[evt.keyCode];
+    });
+    init(); // initiate game objects
+    // game loop function
+    var loop = function () {
+        update();
+        draw();
+        window.requestAnimationFrame(loop, canvas);
+    };
+    window.requestAnimationFrame(loop, canvas);
+}
+function draw() {
+    ctx.fillStyle = "#f0f";
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    ctx.save();
+    ctx.fillStyle = "#000";
+    
+    player.draw();
+    
+    
+    ai.draw();
+    // draw the net
+    var w = 4;
+    var x = (WIDTH - w) * 0.5;
+    var y = 0;
+    var step = HEIGHT / 20; // how many net segments
+    while (y < HEIGHT) {
+        ctx.fillRect(x, y + step * 0.25, w, step * 0.5);
+        y += step;
+    }
+    ctx.restore();
+}
+
+function update() {
+    
+    player.update();
+    ai.update();
+}
+
+
+
+function Paddle(x, y, ai) {
+    this.x = x;
+    this.y = y;
+    this.width = 40;
+    this.height = 200;
+    this.isAi = ai;
+    this.draw = function () {
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+       
+    };
+    this.update = function (y) {
+        this.y = y;
+        this.y = Math.max(Math.min(this.y, HEIGHT - this.height), 0);
+    };
+}
+$(document).ready(function () {
+    main();
+
+});
+
+
