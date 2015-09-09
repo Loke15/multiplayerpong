47f051018585ae50924f4c9515f88fe5ccd0922d
Simon <Simon@Simon-PC>
2015-09-09 15:43:58 +0200

    movement on AI

--- js/newpong.js
+++ js/newpong.js
@@ -1,7 +1,8 @@
-var WIDTH = 700, HEIGHT = 600, pi = Math.PI, UpArrow = 38, DownArrow = 40, canvas, ctx, keystate,speed=3;
+var WIDTH = 700, HEIGHT = 600, pi = Math.PI, UpArrow = 38, DownArrow = 40, canvas, ctx, keystate, speed = 3;
 var player = new Paddle(0, 0, false);
-var ai = new Paddle(0, WIDTH - 40, true);
-
+var ai = new Paddle(WIDTH - 40, HEIGHT / 2, true);
+var ball = new Ball();
+//X,Y
 
 function init() {
 
@@ -33,9 +34,10 @@ function main() {
 function draw() {
     ctx.fillStyle = "#f0f";
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
-    
+
     ctx.fillStyle = "#000";
-    
+
+    ball.draw()
     player.draw();
     ai.draw();
     // draw the net
@@ -51,14 +53,17 @@ function draw() {
 }
 
 function update() {
-    if(keystate[DownArrow]){
-        player.update(player.y+speed);
-    }else if(keystate[UpArrow]){
-        player.update(player.y-speed);
+    if (keystate[DownArrow]) {
+        player.update(player.y + speed);
+        ai.update(ai.y-speed);
+    } else if (keystate[UpArrow]) {
+        player.update(player.y - speed);
+        ai.update(ai.y+speed);
     }
+
+
     
-    
-    //ai.update();
+    //ball.update();
 }
 
 
@@ -71,17 +76,37 @@ function Paddle(x, y, ai) {
     this.isAi = ai;
     this.draw = function () {
         ctx.fillRect(this.x, this.y, this.width, this.height);
-        console.log("x" + this.x + "y" + this.y + "width" +this.width + "height"+this.height);
+        console.log("x" + this.x + "y" + this.y + "width" + this.width + "height" + this.height);
     };
     this.update = function (y) {
-        
-        this.y=y;
+
+        this.y = y;
         this.y = Math.max(Math.min(this.y, HEIGHT - this.height), 0);
     };
 }
+
+function Ball() {
+    this.x = WIDTH / 2;
+    this.y = HEIGHT / 2;
+    this.vel = null
+
+    this.side = 20;
+    this.speed = 12;
+
+    this.draw = function () {
+        ctx.fillRect(this.x, this.y, this.side, this.side);
+    }
+    this.update = function () {
+
+
+    }
+}
+
+
 $(document).ready(function () {
     main();
 
 });
+
 
 
